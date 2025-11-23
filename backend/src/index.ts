655591762d757579416415058d851a7b3bf4111e
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-11-23 22:16:02 +0530

    initilized frontend and backned architecture

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors"
+
+const app = express()
+
+const port = 4500
+
+app.use(cors({
+    origin:'*',
+}))
+
+app.use(express.urlencoded({extended:true,limit:"50mb"}))
+
+
+app.get("/",(req,res)=>{
+    res.send("hii from server !!")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
