10daf1ac345dc948ab6f93c9c19edf1fa9b825ff
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-11-24 13:48:31 +0530

    containerize the mern application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,30 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
 const port = 4500;
-
+const dbConnection = async () => {
+  try {
+    await mongoose.connect("mongodb://mongo:27017/dev-ops-db");
+    console.log("db connected");
+  } catch (error:any) {
+    console.log("db connection failed", error.message);
+  }
+};
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
   })
 );
+
+dbConnection()
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hii from server !!");
+  res.send("hii from server and its working good !!");
 });
 
 app.listen(port, () => {
