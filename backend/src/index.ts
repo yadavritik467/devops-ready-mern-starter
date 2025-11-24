ec3c1ead2cc8d10a7c69082b7c9350c400fe0c02
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-11-24 12:10:40 +0530

    formatted backned file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,22 @@
-import express from "express";
-import cors from "cors"
+import express, { type Request, type Response } from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-const port = 4500
+const port = 4500;
 
-app.use(cors({
-    origin:'*',
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(express.urlencoded({extended:true,limit:"50mb"}))
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("hii from server !!");
+});
 
-app.get("/",(req,res)=>{
-    res.send("hii from server !!")
-})
-
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
