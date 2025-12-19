06f2df3b99b92a5d61b3c3bab48cd04b188d18f7
yadavritik467 <yadavritik467>
2025-12-19 15:25:21 +0000

    chore(backend): apply prettier formatting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,48 +1,34 @@
-import express, { type Request, type Response } from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express, { type Request, type Response } from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
-const app = express();
+const app = express()
 
-
-
-
-
-
-const port = 4500;
-
-
-
-
-
+const port = 4500
 
 const dbConnection = async () => {
   try {
+    await mongoose.connect('mongodb://mongo:27017/dev-ops-db')
 
-
-
-    await mongoose.connect("mongodb://mongo:27017/dev-ops-db");
-
-
-        console.log("db connected");
-  } catch (error:any) {
-    console.log("db connection failed", error.message);
+    console.log('db connected')
+  } catch (error: any) {
+    console.log('db connection failed', error.message)
   }
-};
+}
 app.use(
   cors({
-       origin: "http://localhost:5173",
+    origin: 'http://localhost:5173',
   })
-);
+)
 
 dbConnection()
 
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-app.get("/", (req: Request, res: Response) => {
-     res.send("hii from server and its working good !!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('hii from server and its working good !!')
+})
 
 app.listen(port, () => {
-  console.log(`server is running on ${port}`);
-});
+  console.log(`server is running on ${port}`)
+})
