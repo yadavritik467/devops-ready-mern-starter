48a4326d1f67c38120009713294f7e71546b3549
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-19 20:36:30 +0530

    added prettier in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,18 +5,32 @@ import mongoose from "mongoose";
 const app = express();
 
 
+
+
+
+
 const port = 4500;
+
+
+
+
+
 const dbConnection = async () => {
   try {
+
+
+
     await mongoose.connect("mongodb://mongo:27017/dev-ops-db");
-    console.log("db connected");
+
+
+        console.log("db connected");
   } catch (error:any) {
     console.log("db connection failed", error.message);
   }
 };
 app.use(
   cors({
-    origin: "http://localhost:5173",
+       origin: "http://localhost:5173",
   })
 );
 
@@ -25,7 +39,7 @@ dbConnection()
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hii from server and its working good !!");
+     res.send("hii from server and its working good !!");
 });
 
 app.listen(port, () => {
